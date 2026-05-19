94353844b18d6b796583c6d3a8edf8c0a70ab84e
ESTEVAO AUGUSTO MOTA DOS SANTOS <124156394+Estevaomota1@users.noreply.github.com>
2026-05-19 12:59:46 -0300

    fix cors localhost

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,24 @@
 <?php
 
 return [
+
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://barber-frontend-tan.vercel.app'],
+
+    'allowed_origins' => [
+        'https://barber-frontend-tan.vercel.app',
+        'http://localhost:5173',
+    ],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
+
     'supports_credentials' => true,
+
 ];
